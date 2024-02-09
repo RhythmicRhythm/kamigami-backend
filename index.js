10d9bf64fd9d96883ed449fc4db59b21d6edb6f0
RhythmicRhythm <Okubadejorh@gmail.com>
2024-02-09 18:13:32 +0100

    +

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // cors
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://seinobodies.xyz", "https://luminous-begonia-03e2fc.netlify.app"],
+    origin: ["http://localhost:5173", "https://kamigami.io", "https://luminous-begonia-03e2fc.netlify.app"],
     credentials: true,
   })
 );
